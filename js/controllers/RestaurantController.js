bab9ea62c77ad26c56f1522d1325e645a4baa7b5
Manuel Herrera <manuel.he@gmail.com>
2013-08-27 17:52:41 -0500

    Retrieve data from server using $http service

--- js/controllers/RestaurantController.js
+++ js/controllers/RestaurantController.js
@@ -1,35 +1,15 @@
 'use strict';
 
 foodMeApp.controller('RestaurantController',
-	function RestaurantController($scope, customer, $location){
+	function RestaurantController($scope, customer, $location, $http){
 
 		//Redirect if user is not registered
 		if(!customer.address){
 			$location.path( "/customer" );
 		}
 
-		$scope.restaurants = [
-		    {
-		      "id":"angular",
-		      "name":"Angular Pizza",
-		      "cuisine":"pizza",
-		      "price":1,
-		      "rating":5,
-		      "description":"Home of the superheroic pizza!"
-		    },{
-		      "id":"tofuparadise",
-		      "name":"BBQ Tofu Paradise",
-		      "cuisine":"vegetarian",
-		      "price":2,
-		      "rating":1,
-		      "description":"Vegetarians, we have your BBQ needs covered. Our home-made tofu skewers and secret BBQ sauce will have you licking your fingers."
-		    },{
-		      "id":"beijing",
-		      "name":"Beijing Express",
-		      "cuisine":"chinese",
-		      "price":2,
-		      "rating":4,
-		      "description":"Fast, healthy, Chinese food. Family specials for takeout or delivery. Try our Peking Duck!"
-		    }
-		];
+		$http.get('/api/restaurant').then(function(response){
+			console.log(response);
+			$scope.restaurants = response.data;
+		});
 });
