b007a1503f18e1784a657853ff9b9c58803b4645
Manuel Herrera <manuel.he@gmail.com>
2013-08-28 16:35:49 -0500

    Replaced Delivery info for a custom directive

--- js/controllers/MenuController.js
+++ js/controllers/MenuController.js
@@ -3,5 +3,4 @@
 foodMeApp.controller('MenuController',function Menucontroller($scope, $routeParams, Restaurant, customer){
 	$scope.menu = Restaurant.get({'restaurantId' : $routeParams.restaurantId});
 	$scope.customer = customer;
-	console.log($scope.menu);
 });
---
+++ js/directives/fmDeliverTo.js
@@ -0,0 +1,8 @@
+'use strict';
+
+foodMeApp.directive('fmDeliverTo',function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'js/directives/fmDeliverTo.html'
+	}
+});
